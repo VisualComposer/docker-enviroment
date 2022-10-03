5b54e37e736a7b0f2db25ed3ac51c5368b0ecc89
OlegApanovich <dollar4444@gmail.com>
2022-10-03 14:00:28 +0300

    Update project make

--- VisualcomposerComDocker/project.mk
+++ VisualcomposerComDocker/project.mk
@@ -11,27 +11,27 @@ updateall:
 updatephp:
 	cd ../$(PROJECT_FOLDER_PLUGIN); php ci/composer.phar update
 checkcodestandardphpmain:
-	cd ../$(PROJECT_FOLDER_PLUGIN); php ci/phpcs.phar --standard=ci/ruleset.xml visualcomposer/
+	cd ../$(PROJECT_FOLDER_PLUGIN); php ci/vendor/bin/phpcs --standard=ci/ruleset.xml plugin-wordpress.php bootstrap visualcomposer/Framework visualcomposer/Helpers visualcomposer/Modules visualcomposer/Application.php visualcomposer/Requirements.php
 checkcodestandardjsmain:
 	cd ../$(PROJECT_FOLDER_PLUGIN); yarn lint "public/**/*.js"
 checkcodestandardjselements:
-	cd ../$(PROJECT_FOLDER_PLUGIN)/devElements; yarn lint
+	cd ../$(PROJECT_FOLDER_PLUGIN)/devElements; yarn eslint
 checkcodestandardphpelements:
 	cd ../$(PROJECT_FOLDER_PLUGIN); php devElements/.github/phpcs.phar --standard=devElements/.github/ruleset.xml devElements
 checkcodestandardjsaddons:
-	cd ../$(PROJECT_FOLDER_PLUGIN)/devAddons; yarn lint
+	cd ../$(PROJECT_FOLDER_PLUGIN)/devAddons; yarn eslint
 checkcodestandardphpaddons:
 	cd ../$(PROJECT_FOLDER_PLUGIN); php devAddons/.github/phpcs.phar --standard=devAddons/.github/ruleset.xml devAddons
 fixcodestandardphpmain:
-	cd ../$(PROJECT_FOLDER_PLUGIN); php ci/phpcbf.phar --standard=ci/ruleset.xml visualcomposer/
+	cd ../$(PROJECT_FOLDER_PLUGIN); ci/vendor/bin/phpcbf --standard=ci/ruleset.xml plugin-wordpress.php bootstrap visualcomposer/Framework visualcomposer/Helpers visualcomposer/Modules visualcomposer/Application.php visualcomposer/Requirements.php
 fixcodestandardphpelements:
 	cd ../$(PROJECT_FOLDER_PLUGIN); php ci/phpcbf.phar --standard=devElements/.github/ruleset.xml devElements
 fixcodestandardphpaddons:
 	cd ../$(PROJECT_FOLDER_PLUGIN); php ci/phpcbf.phar --standard=devAddons/.github/ruleset.xml devAddons
 installmain:
-	cd ../$(PROJECT_FOLDER_PLUGIN); git remote add upstream git@github.com:VisualComposer/builder.git; git remote set-url --push upstream no_push;yarn install --ignore-engines; yarn build; php ci/composer.phar update; bash tools/elements/buildScript.sh; cp backend/env-dev.php ../$(PROJECT_FOLDER)/wp-content/plugins/builder
+	cd ../$(PROJECT_FOLDER_PLUGIN); git remote add upstream git@github.com:VisualComposer/builder.git; git remote set-url --push upstream no_push; yarn install --ignore-engines; yarn build; php ci/composer.phar update; bash tools/elements/buildScript.sh; cp backend/env-dev.php ../$(PROJECT_FOLDER)/wp-content/plugins/builder
 installelements:
-	cd ../$(PROJECT_FOLDER_PLUGIN); git clone git@github.com:VisualComposer/devElements.git; cd devElements; yarn install --ignore-engines; bash .hubtools/buildScript.sh
+	cd ../$(PROJECT_FOLDER_PLUGIN); git clone git@github.com:VisualComposer/devElements.git; cd devElements; cp env-dev.php ../; yarn install --ignore-engines; bash .hubtools/buildScript.sh
 installaddons:
 	cd ../$(PROJECT_FOLDER_PLUGIN); git clone git@github.com:VisualComposer/devAddons.git; cd devAddons; bash .hubtools/buildScript.sh
 installpremium:
